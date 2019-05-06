100935fc0698cf6ae26efd1c96302423443c40a2
yurii <yurii.besedin@applitools.com>
2019-05-06 15:11:57 +0300

    eyesCheckWindow

--- test/check-window.spec.js
+++ test/check-window.spec.js
@@ -11,28 +11,12 @@ describe('EyesServiceTest', () => {
 
   it('checkWindow', () => {
     try {
-      browser.checkWindow('main');
+      browser.eyesCheckWindow('main');
     } catch (e) {
       console.error(e);
     }
   });
 
 
-  it('check', () => {
-    try {
-      browser.check('main 2', Target.window());
-    } catch (e) {
-      console.error(e);
-    }
-  });
-
-
-  it('checkFrame', () => {
-    try {
-      browser.checkFrame('frame1', null, 'frame1');
-    } catch (e) {
-      console.error(e);
-    }
-  });
 
 });
